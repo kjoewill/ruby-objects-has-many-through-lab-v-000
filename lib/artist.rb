0216a47d85e3eb724984e84edd4845a678ecb160
Kevin W <kjoewill@gmail.com>
2018-08-29 13:56:50 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,27 @@
+class Artist 
+  
+  @@all = []
+  
+  def self.all
+    @@all
+  end
+  
+  def initialize(name)
+    @name = name
+    @all << self
+  end
+  
+  def new_song(name, genre)
+    song = song.new(name, genre)
+    song.artist = self
+  end
+  
+  def songs 
+    Songs.all.select { |e| each.artist == self }
+  end
+  
+  def genres 
+    self.songs.collect { |e| e.genre }
+  end
+  
+end
